b40903691873acf890824433321b855c03e4ea3f
Amr Azouz <amrsalahazouz@gmail.com>
2023-02-01 02:40:20 +0200

    Hanazel l shar7 2orayeb waiitt

---
+++ morydoro.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Users\Azouz\AppData\Local\Programs\Python\Python39\python.exe" "C:\Python\Projects\Morydoro\morydoro"%*
---
+++ morydoro/__main__.py
@@ -0,0 +1,4 @@
+from morydoro import main
+
+if __name__ == "__main__":
+    main()
